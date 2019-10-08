cd9801a395b0a5f7aad090a53c8dbc5a183b5846
Devon Hubert <devonhubertmusic@gmail.com>
2019-10-08 13:31:06 -0700

    Added Java Runner Class and Makefile

---
+++ PlanitRunner.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class PlanitRunner
+{
+//Fields:
+
+
+    public static void main(String[] args) {
+        
+        System.out.println("It Works!"); //Test
+
+    }
+
+//Methods and stuff:
+
+}
