d8eea925f1f26c87537ff9f8dc06c8a85673eeef
Devon Hubert <devonhubertmusic@gmail.com>
2019-10-11 16:42:47 -0700

    Added FileWriter class

---
+++ FileWriter.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+
+public class FileWriter {
+
+    public void writeToFile(String text) throws java.io.IOException
+    {
+        String userHomeFolder = System.getProperty("user.home");
+        File textFile = new File(userHomeFolder + "/Desktop/", "Database.txt");
+        
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(
+        (textFile), true), "utf-8"))) {
+                  writer.write(text);
+        }
+    }
+        
+}
