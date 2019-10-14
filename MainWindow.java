ccf08e2df3425b4c732d02004813ccac3a3e3f6f
Sharon Wong <swong56@ucsc.edu>
2019-10-13 17:48:29 -0700

    Added new window function

--- MainWindow.java
+++ MainWindow.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.Component;
+import javax.swing.*;
 import javax.swing.JComboBox;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -9,9 +10,10 @@ import javax.swing.JLabel;
 public class MainWindow extends Frame implements WindowListener, ActionListener
 {
     private Button b;
+    private newWindow genWindow = new newWindow();
     private TextField text = new TextField(25);
     private ArrayList<Activity> databaseCopy;
-    
+
     public MainWindow(String title) {
         super(title);
         setLayout(new FlowLayout());
@@ -22,51 +24,52 @@ public class MainWindow extends Frame implements WindowListener, ActionListener
         text.setFont(new Font("Helvetica", Font.ITALIC, 30));
         text.setEditable(false);
         text.setText("        Welcome to Plan-it!");
-        
+
         JLabel picture = new JLabel(new ImageIcon("planit.jpg"));
         add(picture);
-        
+
         //Must be a better way to do spacing, haha
         JLabel label0 = new JLabel("                                  " +
         "                                                             ");
         add(label0);
-        
+
         Button b = new Button("Generate Activity Plan");
         add(b);
         b.addActionListener(this);
-        
+
         //Must be a better way to do spacing, haha
         JLabel label1 = new JLabel("                                  " +
         "                                                             ");
         add(label1);
-        
+
         JLabel label2 = new JLabel("Developed by Team Rocket");
         add(label2);
         label2.setFont(new Font("Helvetica", Font.PLAIN, 14));
     }
-    
+
     public void updateDatabase(ArrayList<Activity> database) {
     	this.databaseCopy = database;
     }
-    
+
     //
     public void actionPerformed(ActionEvent e) {
     	double availableTime = 0.0;
     	double availableMoney = 0.0;
-    
+
         //prompt user for current time and money available and update
         //then...
-        
+
         Plan myPlan = new Plan();
         myPlan.generatePlan(databaseCopy, availableTime, availableMoney);
-        
+
         //then print result in new window
+        genWindow.setVisible(true);
     }
-    
+
     /**
      * Handles the closing of the Main window, re-setting settings to
      * their defaults
-     * 
+     *
      * @param e The WindowEvent triggered by closing the Main window
      */
     public void windowClosing(WindowEvent e) {
