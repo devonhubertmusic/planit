0c11988f745d6d35a91f5f9fc24abe9378f0f1a9
gdjimene <lupej59@me.com>
2019-10-16 09:39:27 -0700

    added dummy buttoms, still have to fix layout

--- newWindow.java
+++ newWindow.java
@@ -2,31 +2,50 @@ import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.Component;
+import java.awt.Container;
+import javax.swing.*;
 import javax.swing.JComboBox;
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JFrame; 
 
+// //going to change the class name asap to activityPlan
 public class newWindow extends Frame implements ActionListener {
 
-    public newWindow(){
+	private Button b;
+	public newWindow(){
         setLayout( new FlowLayout());
-
         setTitle("Plan Generator");
         setSize(500, 350);
         setVisible(false);
-
-        addWindowListener(new WindowAdapter()
+	
+      addWindowListener(new WindowAdapter()
       {
       public void windowClosing(WindowEvent e)
       {
          dispose();
-      }
-      });
-
-      JLabel label3 = new JLabel("Testing Window");
-      add(label3);
+     }
+ });
+     
+//not sure what should be the title
+      JLabel label3 = new JLabel("Activity Plan");
+     add(label3);
       label3.setFont(new Font("Helvetica", Font.PLAIN, 25));
-    }
+label3.setAlignmentY(Component.CENTER_ALIGNMENT);
+
+ //dummy buttons!
+	JButton save = new JButton("Save Plan");
+save.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+	add(save);
+	//save.addActionListener();
+	add(Box.createVerticalGlue());
+	
+	JButton print = new JButton("Print Plan");
+	print.setAlignmentX(Component.CENTER_ALIGNMENT);
+	add(print);
+
+ }
 
     @Override
     public void actionPerformed(ActionEvent evt){
