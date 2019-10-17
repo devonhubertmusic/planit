4dbee47397d14bd6d99eb8051e195705f77e420c
Sharon Wong <swong56@ucsc.edu>
2019-10-17 00:58:52 -0700

    Added edit button

--- currentActivities.java
+++ currentActivities.java
@@ -1,25 +1,38 @@
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.Component;
+import java.awt.Container;
+import javax.swing.*;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JButton;
 
 public class currentActivities {
-    JFrame f;
+    JFrame f = new JFrame();
     JTable j;
+    private Button b;
     ArrayList<Activity> database;
 
     currentActivities()
     {
-        f = new JFrame();
+        f.setLayout( new FlowLayout());
         f.setTitle("Current Activities");
+        JButton edit = new JButton("Edit");
+		    edit.setBounds(100,100,140, 40);
+		    f.add(edit);
 	    this.database = PlanitRunner.database;
+
 
         // Test Data to be displayed in the JTable
         int rowSize = database.size();
         int columnSize = 3;
-        
+
         String[][] data = new String[rowSize][columnSize];
-        
+
         //Fill table with corresponding data from PlanitRunner.database
         //REPLACE WITH DATA FROM MYSQL DATABASE
         for(int row = 0; row < rowSize; row++) {
@@ -28,7 +41,7 @@ public class currentActivities {
                 if(column == 1) {
 		    data[row][column - 1] = "" + temp.getName();
 		} else if(column == 2) {
-            
+
             //convert raw time in minutes to hours, minutes, etc.
             String timeString = "";
             double rawTime = temp.getIdealTime();
@@ -47,7 +60,7 @@ public class currentActivities {
             if(minutes > 0) {
                 timeString += "" + minutes + " minutes";
             }
-            
+
             data[row][column - 1] = timeString;
 		} else if(column == 3) {
             double cost = temp.getMaxCost();
@@ -70,7 +83,7 @@ public class currentActivities {
 
         JScrollPane sp = new JScrollPane(j);
         f.add(sp);
-        f.setSize(500, 200);
+        f.setSize(500, 600);
         f.setVisible(true);
     }
 }
