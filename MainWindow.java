6a25a3d92e6d1c849891a4588ae0b8e3317755da
Shayan Shaikh <shayanshaikh@Shayans-MacBook-Pro-2.local>
2019-10-29 21:01:19 -0700

    updated main window

--- MainWindow.java
+++ MainWindow.java
@@ -7,36 +7,25 @@ import javax.swing.JComboBox;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import java.io.*;
+import java.sql.*;
 
 public class MainWindow extends JFrame implements WindowListener, ItemListener
-{
-    //New activity information
-    public String newActName;
-    public double newActMaxTime;
-    public double newActIdealTime;
-    public double newActMaxCost;
-    
+{   
     //User availablity information
     public double availableTime;
     public double availableMoney;
     
     //copy of the database ArrayList, REPLACE WITH MySQL FUNCTIONALITY
     private ArrayList<Activity> databaseCopy;
-    
+
     //Dropdown boxes for user input
-    private JComboBox c1, c2, c3, c4, c5;
+    private JComboBox timeSelector, moneySelector;
     
     //Labels to display maxTime, idealTime, maxCost, and availableMoney user input
-    private JLabel max1, idealt1, maxc1, time, money;
+    private JLabel time, money;
 
     public MainWindow(String title) {
         super(title); //Add title to window
-        
-        //Initialize data fields
-        newActName = "";
-        newActMaxTime = 30.0;
-        newActIdealTime = 30.0;
-        newActMaxCost = 0.0;
         
         availableTime = 30.0;
         availableMoney = 0.0;
@@ -75,124 +64,12 @@ public class MainWindow extends JFrame implements WindowListener, ItemListener
 
         add(Box.createVerticalGlue());
 
-        //Get data for new activity
-        JLabel newActivity = new JLabel("Enter a New Activity:");
-        newActivity.setAlignmentX(Component.CENTER_ALIGNMENT);
-        add(newActivity);
-        newActivity.setForeground(Color.WHITE);
-        newActivity.setFont(new Font("Helvetica", Font.BOLD, 20));
-
-        JPanel inputName = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JTextField textField = new JTextField();
-        textField.setPreferredSize(new Dimension(150, 25));
-        JLabel activityField = new JLabel("Activity name:");
-        JLabel activityName = new JLabel("");
-        inputName.add(activityField);
-        inputName.add(textField);
-        inputName.add(activityName);
-        activityField.setForeground(Color.WHITE);
-        activityField.setFont(new Font("Helvetica", Font.PLAIN, 16));
-        activityName.setForeground(Color.WHITE);
-        activityName.setFont(new Font("Helvetica", Font.PLAIN, 16));
-        add(inputName);
-        inputName.setOpaque(false);
-
         //time options (in hours)
-        String s1[] = { "0.5", "1", "1.5", "2", "2.5", "3", "3.5", "4", "4.5", "5", "5.5", "6" }; 
-        String[] n1 = new String[100];
+        String timeOptions[] = { "0.5", "1", "1.5", "2", "2.5", "3", "3.5", "4", "4.5", "5", "5.5", "6" }; 
+        String[] costOptions = new String[100];
         for (int i = 0, j = 0; i < 100; ++i, j+=5) {
-            n1[i] = Integer.toString(j);
+            costOptions[i] = Integer.toString(j);
         }
-        // create checkbox 
-        c1 = new JComboBox(s1); 
-        c2 = new JComboBox(s1);
-        c3 = new JComboBox(n1);
-        
-        // add ItemListener 
-        c1.addItemListener(this);
-        c2.addItemListener(this);
-        c3.addItemListener(this);
-        
-        //LABELS:
-        
-        //maxTime input
-        JPanel inputMax = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JLabel max = new JLabel("Max time for activity:"); 
-        max1 = new JLabel("half hour selected");
-        inputMax.add(max);
-        inputMax.add(c1);
-        inputMax.add(max1);
-        max.setForeground(Color.WHITE);
-        max.setFont(new Font("Helvetica", Font.PLAIN, 16));
-        max1.setForeground(Color.WHITE);
-        max1.setFont(new Font("Helvetica", Font.PLAIN, 16));
-        add(inputMax);
-        inputMax.setOpaque(false);
-        
-        //idealTime input
-        JPanel inputIdeal = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JLabel idealt = new JLabel("Ideal time for activity:"); 
-        idealt1 = new JLabel("half hour selected");
-        inputIdeal.add(idealt);
-        inputIdeal.add(c2);
-        inputIdeal.add(idealt1);
-        idealt.setForeground(Color.WHITE);
-        idealt.setFont(new Font("Helvetica", Font.PLAIN, 16));
-        idealt1.setForeground(Color.WHITE);
-        idealt1.setFont(new Font("Helvetica", Font.PLAIN, 16));
-        add(inputIdeal);
-        inputIdeal.setOpaque(false);
-
-        //maxCost input
-        JPanel inputCost = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JLabel maxc = new JLabel("Max cost for activity:"); 
-        maxc1 = new JLabel("0 dollars selected");
-        inputCost.add(maxc);
-        inputCost.add(c3);
-        inputCost.add(maxc1);
-        maxc.setForeground(Color.WHITE);
-        maxc.setFont(new Font("Helvetica", Font.PLAIN, 16));
-        maxc1.setForeground(Color.WHITE);
-        maxc1.setFont(new Font("Helvetica", Font.PLAIN, 16));
-        add(inputCost);
-        inputCost.setOpaque(false);
-
-        //"Save Activity" Button
-        JButton saveb = new JButton("Save Current Activity");
-        saveb.addActionListener( new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                //Activity name entered
-                newActName = textField.getText();
-                
-                //Create new activity, and set fields based on user input
-                Activity temp = new Activity();
-                temp.setName(newActName);
-                temp.setMaxTime(newActMaxTime);
-                temp.setIdealTime(newActIdealTime);
-                temp.setMaxCost(newActMaxCost);
-                
-                if(PlanitRunner.database.contains(temp)) {
-                    //Checks whether activity already exists in the database
-                    activityName.setText("Activity has already been saved");
-                } else if(newActMaxTime < newActIdealTime) {
-                    //Ensures maxTime is larger than idealTime
-                    activityName.setText("Max time must be larger than ideal time");
-                } else if(newActName == null || newActName.isEmpty()) {
-                    //Checks if user left the activity name field blank
-                    activityName.setText("Please Enter a Name For Your Activity");
-                } else {
-                    //Add Activity to the database!
-                    PlanitRunner.database.add(temp);
-                    activityName.setText("Activity Saved!");
-                    c1.setSelectedIndex(0);
-                    c2.setSelectedIndex(0);
-                    c3.setSelectedIndex(0);
-                    textField.setText("");
-                }
-            }
-        });
-        saveb.setAlignmentX(Component.CENTER_ALIGNMENT);
-        add(saveb);
 
         //Get user information to generate new activity plan
         JPanel inputPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
@@ -209,24 +86,24 @@ public class MainWindow extends JFrame implements WindowListener, ItemListener
         newActivityPlan.setForeground(Color.WHITE);
         newActivityPlan.setFont(new Font("Helvetica", Font.BOLD, 20));
         
-        c4 = new JComboBox(s1);
-        c4.addItemListener(this);
+        timeSelector = new JComboBox(timeOptions);
+        timeSelector.addItemListener(this);
         JLabel userTime = new JLabel("Select amount of free time:");
         time = new JLabel("half hour selected");
         inputPanel.add(userTime);
-        inputPanel.add(c4);
+        inputPanel.add(timeSelector);
         inputPanel.add(time);
         userTime.setForeground(Color.WHITE);
         userTime.setFont(new Font("Helvetica", Font.PLAIN, 16));
         time.setForeground(Color.WHITE);
         time.setFont(new Font("Helvetica", Font.PLAIN, 16));
         
-        c5 = new JComboBox(n1);
-        c5.addItemListener(this);
+        moneySelector = new JComboBox(costOptions);
+        moneySelector.addItemListener(this);
         JLabel userMoney = new JLabel("     Select current budget:");
         money = new JLabel("0 dollars selected"); 
         inputPanel.add(userMoney);
-        inputPanel.add(c5);
+        inputPanel.add(moneySelector);
         inputPanel.add(money);
         userMoney.setForeground(Color.WHITE);
         userMoney.setFont(new Font("Helvetica", Font.PLAIN, 16));
@@ -239,6 +116,15 @@ public class MainWindow extends JFrame implements WindowListener, ItemListener
 
         JPanel viewButtons = new JPanel(new FlowLayout(FlowLayout.CENTER));
         
+        //"Add Activity" button
+        JButton addb = new JButton("Add New Activity");
+        addb.addActionListener( new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                new addActivity();
+            }
+        });
+        viewButtons.add(addb);
+
         //"Generate Activity Plan" button
         JButton b = new JButton("Generate Activity Plan");
         b.addActionListener( new ActionListener() {
@@ -269,52 +155,16 @@ public class MainWindow extends JFrame implements WindowListener, ItemListener
         label3.setFont(new Font("Helvetica", Font.PLAIN, 14));
         add(Box.createVerticalGlue());
     }
+    
+    public void updateDatabase(ArrayList<Activity> database) {
+        this.databaseCopy = database; //REPLACE WITH CALL TO MySQL
+    }
 
     public void itemStateChanged(ItemEvent e) 
     { 
         // if the state combobox is changed 
-        if (e.getSource() == c1) {
-            String selection = "" + c1.getSelectedItem();
-            double selectionDouble = Double.parseDouble(selection);
-            String hours = "";
-            if(selectionDouble == 1.0) {
-                hours = "hour";
-            } else if(selectionDouble == 0.5) {
-                selection = "";
-                hours = "half hour";
-            } else {
-                hours = "hours";
-            }
-            max1.setText(selection + " " + hours + " selected");
-            
-            //Update new activity maxTime variable based on current selection
-            newActMaxTime = 60.0 * Double.parseDouble("" + c1.getSelectedItem());
-        }
-        else if (e.getSource() == c2) {
-            String selection = "" + c2.getSelectedItem();
-            double selectionDouble = Double.parseDouble(selection);
-            String hours = "";
-            if(selectionDouble == 1.0) {
-                hours = "hour";
-            } else if(selectionDouble == 0.5) {
-                selection = "";
-                hours = "half hour";
-            } else {
-                hours = "hours";
-            }
-            idealt1.setText(selection + " " + hours + " selected");
-            
-            //Update new activity idealTime variable based on current selection
-            newActIdealTime = 60.0 * Double.parseDouble("" + c2.getSelectedItem());
-        }
-        else if (e.getSource() == c3) {
-            maxc1.setText(c3.getSelectedItem() + " dollars selected");
-            
-            //Update new activity maxCost variable based on current selection
-            newActMaxCost = Double.parseDouble("" + c3.getSelectedItem());
-        }
-        else if (e.getSource() == c4) {
-            String selection = "" + c4.getSelectedItem();
+        if (e.getSource() == timeSelector) {
+            String selection = "" + timeSelector.getSelectedItem();
             double selectionDouble = Double.parseDouble(selection);
             String hours = "";
             if(selectionDouble == 1.0) {
@@ -328,19 +178,16 @@ public class MainWindow extends JFrame implements WindowListener, ItemListener
             time.setText(selection + " " + hours + " selected");
             
             //Update user's availableTime variable based on current selection
-            availableTime = 60.0 * Double.parseDouble("" + c4.getSelectedItem());
+            availableTime = 60.0 * Double.parseDouble("" + timeSelector.getSelectedItem());
         }
-        else if (e.getSource() == c5) {
-            money.setText(c5.getSelectedItem() + " dollars selected");
+        else if (e.getSource() == moneySelector) {
+            money.setText(moneySelector.getSelectedItem() + " dollars selected");
             
             //Update user's availableMoney variable based on current selection
-            availableMoney = Double.parseDouble("" + c5.getSelectedItem());
+            availableMoney = Double.parseDouble("" + moneySelector.getSelectedItem());
         }
     }
-    
-    public void updateDatabase(ArrayList<Activity> database) {
-        this.databaseCopy = database; //REPLACE WITH CALL TO MySQL
-    }
+
 
     //Handles the closing of the Main window, re-setting settings to their defaults
     public void windowClosing(WindowEvent e)
