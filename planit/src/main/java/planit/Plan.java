f974424bd151f028a781e0e3f1b712a21005e30e
Devon Hubert <devonhubertmusic@gmail.com>
2019-11-19 16:38:10 -0800

    Working on temporary algorithm

--- planit/src/main/java/planit/Plan.java
+++ planit/src/main/java/planit/Plan.java
@@ -25,25 +25,75 @@ public class Plan {
 	}
 	
 	//Methods
-    public void generatePlan_New(ArrayList<Activity> database, double availableTime, double
+    public void generatePlan_TEMP(ArrayList<Activity> database, double availableTime, double
     availableMoney, double costPercent) {
         initializeActivityListAndUpdate();
         ArrayList<Activity> activitiesSortedByExpensiveness = sortByExpensiveness(copyDatabase(database)); //Do before calling?
-        double averageExpensiveness = findAverageExpensiveness(activitiesSortedByExpensiveness);
+        double averageExpensiveness = findAverageExpensiveness(activitiesSortedByExpensiveness); //Do before calling?
         double desiredExpensiveness = availableMoney/availableTime;
         
         if(availableTime > findMaxPossibleActivityTime(activitiesSortedByExpensiveness)) {
-            displayErrorMessage("Unable to generate a long enough plan with the given activities");
+            displayErrorMessage("Unable to generate a long enough plan with the given activities, please choose a "
+                                + "smaller time, or add longer activities!");
         } else if(availableMoney < findMinPossibleActivityCost(activitiesSortedByExpensiveness)){
             displayErrorMessage("Not enough money for any given activity");
         } else {
-            //Generate plan using the sorted list (will work on more tomorrow)
+            //Generate plan using the sorted list
+            ArrayList<Activity> possibleActivities = findPossibleActivities(activitiesSortedByExpensiveness,
+                                averageExpensiveness, desiredExpensiveness, availableTime, availableMoney);
+            double totalTime = 0.0;
+            double totalCost = 0.0;
+            double minCost = costPercent * availableMoney;
+                while(possibleActivities.size() > 0) { 
+                    double remainingTime = availableTime - totalTime;
+                    double remainingMoney = availableMoney - totalCost;
+                
+                    Random rn = new Random();
+                    int randomIndex = rn.nextInt(possibleActivities.size());
+                    Activity currentActivity = possibleActivities.get(randomIndex);
+                    if(currentActivity.getIdealTime() <= remainingTime
+                        && currentActivity.getMaxCost() <= remainingMoney) {
+                        activityList.add(currentActivity);
+                        totalTime += currentActivity.getIdealTime();
+                        totalCost += currentActivity.getMaxCost();
+                    }
+                    possibleActivities.remove(randomIndex);
+                }
+                double remainingTime = availableTime - totalTime;
+                double remainingMoney = availableMoney - totalCost;
+            
+                double potentialStretch = 0.0;
+                for(int i = 0; i < activityList.size(); i++) {
+                    Activity temp = activityList.get(i);
+                    potentialStretch += temp.getTimeGap();
+                }
+            
+                //% DECIDES HOW FAR FROM IDEAL TIME WE ARE WILLING TO STRETCH
+                double maxStretch = (1/activityList.size()) * potentialStretch;
+            
+                if(maxStretch >= remainingTime && (totalCost >= minCost || availableMoney == 0)) {
+                    double stretchPercent = remainingTime/potentialStretch;
+                    for(int i = 0; i < activityList.size(); i++) {
+                        Activity temp = activityList.get(i);
+                        temp.setActualTime(temp.getIdealTime() + (stretchPercent * temp.getTimeGap()));
+                    }
+                } else {
+                    displayErrorMessage("Something went wrong, blame Devon");
+                }
+            
+            
         }
+    }
+    
+    public ArrayList<Activity> findPossibleActivities(ArrayList<Activity> activityList, double averageExpensiveness,
+                               double desiredExpensiveness, double availableTime, double availableMoney) {
+        return new ArrayList<Activity>(); //CHANGE
     }
     
     public void displayErrorMessage(String errorMessage) {
         Activity message = new Activity();
         message.setName(errorMessage); //Replace with pop-up
+        this.activityList = new ArrayList<Activity>();
         this.activityList.add(message);
     }
     
@@ -150,6 +200,7 @@ public class Plan {
             }
         }
 	}
+    
     
     //Makes a deep copy of an Arraylist<Activity>
     public ArrayList<Activity> copyDatabase(ArrayList<Activity> database) {
