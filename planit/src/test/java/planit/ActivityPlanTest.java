fcc33a0a79f66fa8da0fd8f785784ac5537fcb90
Sebastian Park <swpark@ucsc.edu>
2019-12-01 22:06:08 -0800

    updated activityList test

--- planit/src/test/java/planit/ActivityPlanTest.java
+++ planit/src/test/java/planit/ActivityPlanTest.java
@@ -14,21 +14,27 @@ import org.junit.Test;
 public class ActivityPlanTest 
 {
     private static ArrayList<Activity> myList;
+    private static ArrayList<Activity> myActivityList;
     private static Plan myPlan;
     private static int testCounter;
+    private static double availableTime;
+    private static double availableMoney;
     
     @BeforeClass
     public static void initActivityPlan() {
-        System.out.println("Test Class - ActivityPlanTest.java\nThis class contains 1 test\n");
-        System.out.println("Initializing activity plan to test plan generator. . .");
+        System.out.println("Test Class - ActivityPlanTest.java\nThis class contains 2 tests");
+        System.out.println("Initializing activity plan to test plan generator. . .\n");
         myPlan = new Plan();
         myList = new ArrayList<Activity>();
-        Activity test1 = new Activity("Test1 - Name", "Misc", 60, 30, 10);
-        Activity test2 = new Activity("Test2 - Name", "Misc", 30, 15, 15);
-        Activity test3 = new Activity("Test3 - Name", "Misc", 90, 45, 20);
+        myActivityList = new ArrayList<Activity>();
+        Activity test1 = new Activity("Juggle", "Misc", 60, 30, 5);
+        Activity test2 = new Activity("Ride a bike", "Misc", 30, 15, 35);
+        Activity test3 = new Activity("Go shopping", "Misc", 90, 45, 0);
         myList.add(test1);
         myList.add(test2);
         myList.add(test3);
+        availableTime = 60.0;
+        availableMoney = 50.0;
         testCounter = 0;
     }
 
@@ -49,15 +55,34 @@ public class ActivityPlanTest
     }
  
     @Test
-
     public void testGetPlanList() {
         System.out.println("Testing generatePlan function");
-        ArrayList<Activity> myActivityList = new ArrayList<Activity>();
-        boolean generated = myPlan.generatePlan(myList, 60.0, 50.0, 0.75);
+        boolean generated = myPlan.generatePlan(myList, availableTime, availableMoney, 0.75);
         if(generated){
             myActivityList = myPlan.getActivityList();
         }
         assertTrue(generated);
+    }
+
+    @Test
+    public void showPlanList(){
+        double totalCost = 0.0;
+        double totalTime = 0.0;
+        System.out.println("Testing plan generator\n");
+        System.out.println("Available money: $" + availableMoney);
+        System.out.println("Available time: " + availableTime + " minutes\n");
+        Activity temp = new Activity();
+        int listSize = myActivityList.size();
+        System.out.println("Activity list generated has " + listSize + " activies");
+        for (int counter = 0; counter < listSize; counter++){
+            temp = myActivityList.get(counter);
+            totalCost += temp.getMaxCost();
+            totalTime += temp.getActualTime();
+            System.out.println("Activity " + (counter + 1) + ": " + temp.getName());
+        }
+        System.out.println("\nTotal cost of plan: $" + totalCost);
+        System.out.println("Total time of plan: " + totalTime + " minutes");
+        assertTrue(totalCost <= availableMoney && totalTime <= availableTime);
     }
 
 
